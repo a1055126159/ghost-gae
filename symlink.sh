e8dad412c4134c78169f3aaffc7f322ff4484487
JesseChen <jessechen@KKBOXdeMacBook-Pro-199.local>
2017-09-24 00:45:16 +0800

    debug msg

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 path="$(pwd)"
+echo $path
+ls -al
+ls -al node_modules
 version="$(ls -t ghost/versions | head -1)"
 target=$path/ghost/versions/$version
 echo $target
 ln -s $target ghost/current
+cd ghost
+ls -al
+cd current
+ls -al
