5b44240eebaff10f01820b75b85668bb0aee0cb2
JesseChen <jessechen@KKBOXdeMacBook-Pro-199.local>
2017-10-07 14:12:05 +0800

    echo path

--- prestart.sh
+++ prestart.sh
@@ -6,7 +6,7 @@ mkdir ghost
 cd ghost
 # ghost install --url=https://hellojcc-178809.appspot.com --no-prompt --no-stack --no-setup-linux-user --no-setup-mysql --no-setup-nginx --no-setup-systemd
 ghost install --url=https://hellojcc-178809.appspot.com --db=sqlite3 --dbpath=./content/data/ghost.db --no-prompt --no-stack --no-setup-linux-user --no-setup --no-setup-mysql --no-setup-nginx --no-setup-systemd
-# echo $path
+echo $path
 cp ../config*.json ./
 ghost setup migrate
 cd content/themes
